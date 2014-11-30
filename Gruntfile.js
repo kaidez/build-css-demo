e861794c90b5b98e292c38bc14c50cb8ce3673bb
kaidez <kai.gittens@gmail.com>
2014-11-29 23:59:04 -0500

    start working on Grunt bowercopy task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,24 @@
 grunt.initConfig({
+  pkg: grunt.file.readJSON('package.json'),
   bowercopy: {
+
+    // don't send messages to saying that Bower components aren't
+    // configured...ignore them instead
     options: {
-      // Task-specific options go here
+      ignore: ['gulp', 'jquery'],
+      runBower: false
     },
-    your_target: {
-      // Target-specific file lists and/or options go here
+
+    // Copy Bootstrap over
+    bscss: {
+
+      // copy to the "css-build" directory
+      options: {
+        destPrefix: 'css-build/'
+      },
+      files: {
+        'bootstrap.css': 'bootstrap/dist/css/bootstrap.css'
+      }
     }
   }
 });
