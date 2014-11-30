cef7c273d6413d5333f6d23afa8a94e6d76f6269
kaidez <kai.gittens@gmail.com>
2014-11-30 00:07:23 -0500

    clean up "grunt bowercopy" task in Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,26 +1,33 @@
-grunt.initConfig({
-  pkg: grunt.file.readJSON('package.json'),
-  bowercopy: {
+module.exports = function(grunt) {
 
-    // don't send messages to saying that Bower components aren't
-    // configured...ignore them instead
-    options: {
-      ignore: ['gulp', 'jquery'],
-      runBower: false
-    },
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
 
-    // Copy Bootstrap over
-    bscss: {
-
-      // copy to the "css-build" directory
+   // start "bowercopy" task
+    bowercopy: {
+      // don't send messages to saying that Bower components aren't
+      // configured...ignore them instead. Also, don't run any bower
+      // tasks when grunt runs this task.
       options: {
-        destPrefix: 'css-build/'
+        ignore: ['gulp', 'jquery'],
+        runBower: false
       },
-      files: {
-        'bootstrap.css': 'bootstrap/dist/css/bootstrap.css'
-      }
-    }
-  }
-});
 
-grunt.loadNpmTasks('grunt-bowercopy');
+      // Copy Bootstrap CCS over
+      bscss: {
+
+        // copy to the "css-build" directory
+        options: {
+          destPrefix: 'css-build/'
+        },
+        files: {
+          'bootstrap.css': 'bootstrap/dist/css/bootstrap.css'
+        }
+      }
+    } // end "bowercopy" task
+  });
+
+  grunt.loadNpmTasks('grunt-bowercopy');
+
+};
