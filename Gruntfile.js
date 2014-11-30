8f4898ce98ce00554d215cea27b10e052f4da2f3
kaidez <kai.gittens@gmail.com>
2014-11-29 23:54:26 -0500

    start Gruntfile.js

---
+++ Gruntfile.js
@@ -0,0 +1,12 @@
+grunt.initConfig({
+  bowercopy: {
+    options: {
+      // Task-specific options go here
+    },
+    your_target: {
+      // Target-specific file lists and/or options go here
+    }
+  }
+});
+
+grunt.loadNpmTasks('grunt-bowercopy');
