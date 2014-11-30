43fa98fdb9d13fe07d3a9c70a92834ff58c92b7c
kaidez <kai.gittens@gmail.com>
2014-11-29 23:43:29 -0500

    start configuring LESS tasks (see message) -create "gulp less" task in gulpfile.js -start .less files in "less" for importing/building out -make sure final build goes to build/css/styles.css

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,15 @@
 // Single var pattern in full effect
 
 var gulp = require('gulp'), // Define gulp
-jade = require('gulp-jade'); // Jade task
+   jade = require('gulp-jade'), // Jade task
+   less = require('gulp-less'), // LESS task
+   path = require('path'); // Needed for LESS
+
 /*
  *  ===================================================================
- *  | START JADE BUILD-OUT |
+ *  | JADE TASK |
  *  ===================================================================
  */
-
-
 var jadeFiles = ['jade/index.jade', 'jade/**/*.jade']; // Jade files
 
 // Output Jade Files to build "index.html" & send it to "build/"
@@ -18,4 +19,17 @@ gulp.task('jade', function () {
     pretty: true
   }))
   .pipe(gulp.dest('build'));
+});
+
+/*
+*  ===================================================================
+*  | LESS TASK |
+*  ===================================================================
+*/
+gulp.task('less', function () {
+  gulp.src('./less/**/*.less')
+  .pipe(less({
+    paths: [ path.join(__dirname, 'less', 'includes') ]
+  }))
+  .pipe(gulp.dest('./public/css'));
 });
