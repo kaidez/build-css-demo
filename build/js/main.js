e20b131cfd6a6b818fc40fdca46338ba17f4c7a3
kaidez <kai.gittens@gmail.com>
2014-12-02 07:44:10 -0500

    remove no-longer-used scrollNav code

--- build/js/main.js
+++ build/js/main.js
@@ -1,17 +1,1 @@
 
-/*
-Scroll nav code starts
- */
-$(".container").scrollNav({
-  showTopLink: false,
-  sectionElem: "div",
-  showHeadline: false,
-  speed: 600,
-  insertTarget: this.headerElements,
-  insertLocation: "prependTo"
-});
-
-
-/*
-Scroll nav code ends
- */
