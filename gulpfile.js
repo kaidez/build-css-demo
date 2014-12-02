0dc510d1c50ee7814f8725009b39f5a90e958853
kaidez <kai.gittens@gmail.com>
2014-12-01 22:26:36 -0500

    make single quotes double quotes in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,23 @@
 // Single var pattern in full effect!!!
 
-var gulp = require('gulp'), // Define gulp
-    jade = require('gulp-jade'), // Jade task
-    uncss = require('gulp-uncss'), // Remove unused css task
-    minifyCSS = require('gulp-minify-css'), // Minify CSS
-    concatCss = require('gulp-concat-css'), // Concatenate CSS
-    csslint = require('gulp-csslint'), // Lint CSS
-    concat = require('gulp-concat'), // For concatenating stuff(?)
-    watch = require('gulp-watch'), // For watching files changes
+var gulp = require("gulp"), // Define gulp
+    jade = require("gulp-jade"), // Jade task
+    uncss = require("gulp-uncss"), // Remove unused css task
+    minifyCSS = require("gulp-minify-css"), // Minify CSS
+    concatCss = require("gulp-concat-css"), // Concatenate CSS
+    csslint = require("gulp-csslint"), // Lint CSS
+    concat = require("gulp-concat"), // For concatenating stuff(?)
+    watch = require("gulp-watch"), // For watching files changes
 
     // Live Reload stuff
-    livereload = require('gulp-livereload'),
-    lr = require('tiny-lr'),
+    livereload = require("gulp-livereload"),
+    lr = require("tiny-lr"),
     server = lr();
 
 // End single var pattern
 
 // Needed to run grunt tasks through gulp
-require('gulp-grunt')(gulp);
+require("gulp-grunt")(gulp);
 
 /*
  *  ===================================================================
@@ -27,7 +27,7 @@ require('gulp-grunt')(gulp);
 var jadeFiles = ["jade/index.jade", "jade/**/*.jade"], // Jade
     lessFiles = ["css-build/*.less", "css-build/**/*.less"], // LESS
     coffeeFiles = ["coffee/*.coffee"], // Coffescript
-    ignoreArray = [];
+    ignoreArray = ["nav"];
 
 /*
  *  ===================================================================
@@ -35,12 +35,12 @@ var jadeFiles = ["jade/index.jade", "jade/**/*.jade"], // Jade
  *  ===================================================================
  */
 // Output Jade Files to build "index.html" & send it to "build/"
-gulp.task('jade', function () {
-  return gulp.src('jade/index.jade')
+gulp.task("jade", function () {
+  return gulp.src("jade/index.jade")
   .pipe(jade({
     pretty: true
   }))
-  .pipe(gulp.dest('build'));
+  .pipe(gulp.dest("build"));
 });
 
 /*
@@ -49,7 +49,7 @@ gulp.task('jade', function () {
  *
  * Running "gulp buildcss" performs the following sequence of tasks...
  *
- * 1. Runs the "gulp less" task that's passed as a "hint"(*)
+ * 1. Runs the "gulp less" task that"s passed as a "hint"(*)
  * 2. Concatenate selected .css files listed in "gulp.src"
  * 3. Remove unused CSS
  * 4. Minify CSS
@@ -59,15 +59,15 @@ gulp.task('jade', function () {
  * https://github.com/gulpjs/gulp/blob/master/docs/API.md
  *  ===================================================================
  */
-gulp.task('buildcss', ['less'], function () {
-  gulp.src(['css-build/bootstrap.css', 'css-build/styles.css'])
+gulp.task("buildcss", ["less"], function () {
+  gulp.src(["css-build/bootstrap.css", "css-build/styles.css"])
   .pipe(concatCss("styles.min.css"))
   .pipe(uncss({
-    html: ['build/index.html'],
+    html: ["build/index.html"],
     ignore: ignoreArray
   })).pipe(minifyCSS({
     keepBreaks: true
-  })).pipe(gulp.dest('build/css/'))
+  })).pipe(gulp.dest("build/css/"))
   .pipe(csslint({
     "important": false,
     "duplicate-background-images": false,
@@ -78,38 +78,38 @@ gulp.task('buildcss', ['less'], function () {
 
 /*
  *  ===================================================================
- *  | 'gulp-grunt' TASKS...RUN GRUNT TASKS VIA GULP!!!! |
+ *  | "gulp-grunt" TASKS...RUN GRUNT TASKS VIA GULP!!!! |
  *  ===================================================================
  */
 // Run the "grunt coffee" task
-gulp.task('coffee', function () {
-  gulp.run('grunt-coffee');
+gulp.task("coffee", function () {
+  gulp.run("grunt-coffee");
 });
 
 // Run the "grunt less" task
-gulp.task('less', function () {
-  gulp.run('grunt-less');
+gulp.task("less", function () {
+  gulp.run("grunt-less");
 });
 
 // BOWERCOPY TASKS
 // Copy over ALL the Bower Components!!!
-gulp.task('bowercopy', function () {
-  gulp.run('grunt-bowercopy');
+gulp.task("bowercopy", function () {
+  gulp.run("grunt-bowercopy");
 });
 
 // Copy over Bootstrap core .css only
-gulp.task('bowerbscss', function () {
-  gulp.run('grunt-bowercopy:bscss');
+gulp.task("bowerbscss", function () {
+  gulp.run("grunt-bowercopy:bscss");
 });
 
 // Copy over jQuery v.1.11.1 only
-gulp.task('bowerjq', function () {
-  gulp.run('grunt-bowercopy:jq');
+gulp.task("bowerjq", function () {
+  gulp.run("grunt-bowercopy:jq");
 });
 
 // Copy over scrollNav library only
-gulp.task('bowerscroll', function () {
-  gulp.run('grunt-bowercopy:scrollnav');
+gulp.task("bowerscroll", function () {
+  gulp.run("grunt-bowercopy:scrollnav");
 });
 
 /*
@@ -120,7 +120,7 @@ gulp.task('bowerscroll', function () {
  *  computer memory...at least, it does in Grunt.
  *  ===================================================================
  */
-gulp.task('watch', function () {
+gulp.task("watch", function () {
 
   /*
    * If either preprocesser files or "build/" files change and the site
@@ -128,9 +128,9 @@ gulp.task('watch', function () {
    * livereload.
    */
   var server = livereload();
-  gulp.watch(jadeFiles, ['jade']);
-  gulp.watch(lessFiles, ['buildcss']);
-  gulp.watch(coffeeFiles, ['coffee']);
+  gulp.watch(jadeFiles, ["jade"]);
+  gulp.watch(lessFiles, ["buildcss"]);
+  gulp.watch(coffeeFiles, ["coffee"]);
   gulp.watch(["build/index.html", "build/css/*.css", "build/js/*.js"],
   function (e) {
     server.changed(e.path);
