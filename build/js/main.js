8ad193bf39628627c8350005d09a9c66853fae9f
kaidez <kai.gittens@gmail.com>
2014-12-04 10:02:08 -0500

    start construting player info off-DOM

--- build/js/main.js
+++ build/js/main.js
@@ -10,18 +10,20 @@ var playersInfo;
 
 $(".cycle-slideshow").cycle({
   log: false
-});
+}, playersInfo = $.getJSON("js/playersInfo.json", function(data) {
+  return $.each(data, function(key, val) {
+    var allInfo, allInfoName, frag;
+    frag = document.createDocumentFragment();
+    allInfo = document.createElement("div");
+    allInfoName = document.createElement("h3");
+    allInfo.setAttribute("id", key);
+    allInfoName.innerHTML = this.name;
+    allInfo.appendChild(allInfoName);
+    console.log(allInfo);
+  });
+}));
 
 $("#chelseaPlayers").on("cycle-before", function(event, optionHash, outgoingSlideEl, incomingSlideEl, forwardFlag) {
   var $targetEl;
-  $targetEl = $(incomingSlideEl).attr("id");
-  return console.log($targetEl);
-});
-
-playersInfo = $.getJSON("js/playersInfo.json", function(data) {
-  var items;
-  items = [];
-  return $.each(data, function(key, val) {
-    return console.log(key, val);
-  });
+  return $targetEl = $(incomingSlideEl);
 });
--- coffee/scrollnav.coffee
+++ coffee/scrollnav.coffee
@@ -8,13 +8,16 @@ Stop those pain-in-the-ass console statements that Cycle2 fires off
 $( ".cycle-slideshow" ).cycle
   log: false
 
+  playersInfo = $.getJSON "js/playersInfo.json", ( data ) ->
+    $.each data, ( key, val ) ->
+      frag = document.createDocumentFragment()
+      allInfo = document.createElement ( "div" )
+      allInfoName = document.createElement ( "h3" )
+      allInfo.setAttribute "id", key
+      allInfoName.innerHTML = this.name
+      allInfo.appendChild allInfoName
+      console.log allInfo
+      return
 
 $( "#chelseaPlayers" ).on "cycle-before", ( event, optionHash, outgoingSlideEl, incomingSlideEl, forwardFlag ) ->
-  $targetEl = $( incomingSlideEl ).attr "id"
-  console.log $targetEl
-
-
-playersInfo = $.getJSON "js/playersInfo.json", ( data ) ->
-  items = []
-  $.each data, ( key, val ) ->
-    console.log key, val
+  $targetEl = $( incomingSlideEl )
