6ded8397ca1075c2bf79c0d19749b7f42a87be93
kaidez <kai.gittens@gmail.com>
2014-12-15 22:53:47 -0500

    update pageres

--- pageres.js
+++ pageres.js
@@ -1,13 +1,13 @@
 var Pageres = require('pageres');
 
-var pageres = new Pageres({delay: 2})
-    .src('kaidez.com', ['480x320', "1024x768"])
-    .dest("screenshots");
+var pageres = new Pageres()
+  .src("localhost:8000", ["480x320"])
+  .dest("screenshots");
 
 pageres.run(function (err) {
-    if (err) {
-        throw err;
-    }
+  if (err) {
+      throw err;
+  }
 
-    console.log('done');
+  console.log("done");
 });
